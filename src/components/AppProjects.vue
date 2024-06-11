d8e02a578c832ec0048b71cf45a7e1da2c874643
christiansaccani <christian.saccani99@gmail.com>
2024-06-11 21:29:58 +0200

    project section done

--- src/components/AppProjects.vue
+++ src/components/AppProjects.vue
@@ -17,32 +17,35 @@ export default {
 <!-- ---------------------------------------------------------------------------------- -->
 
 <template>
-<section class="py-4">
+<section class="py-5">
+
+    <h2 class="mb-5">I miei Progetti</h2>
 
     <div class="card_container d-flex row w-100 justify-content-center gap-5">
-
-        <div class="card px-1 py-3 rounded-4" style="width: 18rem;">
-            <img class="card-img-top" src="D:\PPCoding\my-portfolio\src\assets\projects\MacBook Air top.png" alt="Boolivery">
-            <!-- dare possibilità di aprire l'immagine -->
-                <div class="card-body">
-                    <h5 class="card-title">Boolivery</h5>
-                    <p class="card-text">Il progetto simula una webapp di food delivery usando <strong>Vite</strong> e <strong>Laravel</strong> per un'esperienza utente rapida, sicura e moderna.</p>
-                    <a href="#" class="btn me-2"><i class="fa-solid fa-play me-2"></i>Video</a>
-                    <a href="#" class="btn"><i class="fa-brands fa-github me-2"></i>Codice</a>
-                </div>
+        <div class="card px-2 py-4 rounded-4" style="width: 20rem;">
+            <img class="card-img-top" src="D:\PPCoding\my-portfolio\src\assets\projects\MacBook Air top.png" alt="Boolivery" data-bs-toggle="modal" data-bs-target="#imageModal">
+            <div class="card-body pb-0">
+                <h4 class="card-title font-weight-bold">Boolivery</h4>
+                <p class="card-text">Progetto finale 'Boolean', webapp di food delivery usando <strong>Vite</strong> e <strong>Laravel</strong> per un'esperienza utente rapida, sicura e moderna.</p>
+                <a href="#" class="btn me-2"><i class="fa-solid fa-play me-2"></i>Video</a>
+                <a href="#" class="btn"><i class="fa-brands fa-github me-2"></i>Codice</a>
+            </div>
         </div>
 
-        <div class="card px-1 py-3 rounded-4" style="width: 18rem;">
-            <img class="card-img-top" src="D:\PPCoding\my-portfolio\src\assets\projects\comics.png" alt="Comics">
-            <!-- dare possibilità di aprire l'immagine -->
-                <div class="card-body">
-                    <h5 class="card-title">Comics</h5>
-                    <p class="card-text">Il progetto simula uno shop di comics attraverso <strong>Vite</strong>, affrontando la gestione di componenti indipendenti</p>
-                    <a href="#" class="btn me-2"><i class="fa-solid fa-play me-2"></i>Video</a>
-                    <a href="#" class="btn"><i class="fa-brands fa-github me-2"></i>Codice</a>
+        <!-- Modal -->
+        <div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg">
+                <div class="modal-content rounded-4">
+                    <div class="modal-header">
+                        <h3 class="modal-title" id="imageModalLabel">Boolivery</h3>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body p-4">
+                        <img src="D:\PPCoding\my-portfolio\src\assets\projects\MacBook Air top.png" alt="Boolivery" class="img-fluid">
+                    </div>
                 </div>
+            </div>
         </div>
-
     </div>
 
 </section>
@@ -64,6 +67,10 @@ section {
     background-color: $primaryColor;
     color: $textColor;
 
+    h2 {
+        color: $backgroundColor;
+    }
+
     .card_container {
         max-width: 1440px;
 
@@ -84,6 +91,7 @@ section {
                 background-color: $backgroundColor;
                 border: 1px solid $primaryColor;
                 transition: all .4s linear;
+
                 &:hover {
                     background-color: $primaryColor;
                     color: $backgroundColor;
@@ -93,4 +101,9 @@ section {
     }
 }
 
+// Modal
+
+.modal {
+    color: $textColor;
+}
 </style>
